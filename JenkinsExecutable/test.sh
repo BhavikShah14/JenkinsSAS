3c82ae392da76583ed7738d9359017fe77ffc7e8
SAS Installer User <sas@sasbap.(none)>
2017-02-27 09:39:50 -0500

    test

--- JenkinsExecutable/test.sh
+++ JenkinsExecutable/test.sh
@@ -1,9 +1,12 @@
-#!/bin/bash
-file = Metadata/environmentProperties.csv
-IFS=','
-read -a headers
-while read -a line; do
-    for i in "${!line[@]}"; do
-        echo "${headers[i]}: ${line[i]}"
-    done
-done < "$file"
+#! /usr/bin/env bash
+for i in $(find $(dirname $(readlink -f $0))/.. -iname "*environmentproperties*.csv")
+do
+	IFS=','
+	read -a headers
+	while read -a line; do
+	    for i in "${!line[@]}"; do
+	        echo "${headers[i]}: ${line[i]}"
+	    done
+	done < "$file"
+done
+unset IFS
