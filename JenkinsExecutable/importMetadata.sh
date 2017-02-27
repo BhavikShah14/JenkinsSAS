633a566dbc50f4eaad1d721862e9f50b97c0d7c2
SAS Installer User <sas@sasbap.(none)>
2017-02-27 06:24:30 -0500

    Copy properties files

--- JenkinsExecutable/importMetadata.sh
+++ JenkinsExecutable/importMetadata.sh
@@ -1,11 +1,12 @@
+#! Explode the spk into a folder with same name as the spk but with extension .spkd
 #! /usr/bin/env bash
 
 for i in $(find $(dirname $(readlink -f $0))/.. -name "*.spk")
 do
 	unzip -o "$i" -d "$i""d"
-	for j in $(find "$i""d" -name "*.spk")
+	for j in $(find "$i""d" -name "*.subprop")
 	do
-		unzip -o "$j" -d "$j""d"
+		cp $j $i.subprop
 	done
 done
 
