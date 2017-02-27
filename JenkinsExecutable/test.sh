147ace3abb0e9dc3ef576e04aea88f5b1dbdd51f
SAS Installer User <sas@sasbap.(none)>
2017-02-27 09:32:03 -0500

    test

--- JenkinsExecutable/test.sh
+++ JenkinsExecutable/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-file = "/Metadata/environmentProperties.csv"
+file = /Metadata/environmentProperties.csv
 IFS=','
 read -a headers
 while read -a line; do
     for i in "${!line[@]}"; do
         echo "${headers[i]}: ${line[i]}"
     done
-done
+done < "$file"
