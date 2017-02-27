d91daeb5614a952c1192e26f088b6868dbf5aaf5
SAS Installer User <sas@sasbap.(none)>
2017-02-27 06:55:11 -0500

    deploy spks in order

--- JenkinsExecutable/importMetadata.sh
+++ JenkinsExecutable/importMetadata.sh
@@ -4,14 +4,53 @@
 for i in $(find $(dirname $(readlink -f $0))/.. -name "*.spk")
 do
 	unzip -o "$i" -d "$i""d"
+	#Copy the sub properties files outside
 	for j in $(find "$i""d" -name "*.subprop")
 	do
 		cp $j $i.subprop
 	done
 done
 
-#Deploy all spk's
-for i in $(find $(dirname $(readlink -f $0))/.. -name "*.spk")
+#Deploy all spk's in the following order
+#1 Roles
+#2 User Groups
+#3 Users
+#4 Servers
+#5 Libraries if separate spk created
+#6 All other spks
+
+#Roles spk
+for i in $(find $(dirname $(readlink -f $0))/.. -name -i "*roles*.spk")
 do
 	${ImportPackagePath}/ImportPackage -profile "SASAdmin" -package "$i" -target / -includeACL -disableX11
 done
+
+#User Groups spk
+for i in $(find $(dirname $(readlink -f $0))/.. -name -i "*usergroups*.spk")
+do
+	${ImportPackagePath}/ImportPackage -profile "SASAdmin" -package "$i" -target / -includeACL -disableX11
+done
+
+#Users spk
+for i in $(find $(dirname $(readlink -f $0))/.. -name -i "*users*.spk")
+do
+	${ImportPackagePath}/ImportPackage -profile "SASAdmin" -package "$i" -target / -includeACL -disableX11
+done
+
+#Servers spk
+for i in $(find $(dirname $(readlink -f $0))/.. -name -i "*servers*.spk")
+do
+	${ImportPackagePath}/ImportPackage -profile "SASAdmin" -package "$i" -target / -includeACL -disableX11
+done
+
+#Libraries spk
+for i in $(find $(dirname $(readlink -f $0))/.. -name -i "*libraries*.spk")
+do
+	${ImportPackagePath}/ImportPackage -profile "SASAdmin" -package "$i" -target / -includeACL -disableX11
+done
+
+for i in $(find $(dirname $(readlink -f $0))/.. -name  "*.spk")
+do
+	${ImportPackagePath}/ImportPackage -profile "SASAdmin" -package "$i" -target / -includeACL -disableX11
+done
+
