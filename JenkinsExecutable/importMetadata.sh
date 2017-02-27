b9820fe31befd108b5ac86b79330c9e84c68f341
SAS Installer User <sas@sasbap.(none)>
2017-02-27 05:29:00 -0500

    Explode spk

--- JenkinsExecutable/importMetadata.sh
+++ JenkinsExecutable/importMetadata.sh
@@ -1,1 +1,12 @@
+#! /usr/bin/env bash
+
+for i in $(find $(dirname $(readlink -f $0))/Metadata/.. -name "*.spk")
+do
+	unzip -o "$i" -d "$i""d"
+	for j in $(find "$i""d" -name "*.spk")
+	do
+		unzip -o "$j" -d "$j""d"
+	done
+done
+
 ${ImportPackagePath}/ImportPackage -profile "SASAdmin" -package "Metadata/Jenkins.spk" -target / -includeACL -disableX11
