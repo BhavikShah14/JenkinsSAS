8b9d18b1bbc700b3ddd82cfe2f737bd59e13dea6
SAS Installer User <sas@sasbap.(none)>
2017-03-01 04:57:30 -0500

    split

--- JenkinsExecutable/importMetadata.sh
+++ JenkinsExecutable/importMetadata.sh
@@ -1,18 +1,4 @@
-source 'exception.sh'
-#Explode the spk into a folder with same name as the spk but with extension .spkd
-
-
-for i in $(find $(dirname $(readlink -f $0))/.. -name "*.spk")
-do
-	unzip -o "$i" -d "$i""d"
-	#Copy the sub properties files outside
-	for j in $(find "$i""d" -name "*.subprop")
-	do
-		cp $j $i.subprop
-		chmod 775 $i.subprop
-	done
-done
-
+./JenkinsExecutable/spk_lib/spk_explode.sh
 
 #Create a HASH for replacing the subprop files
 #Uses the environment properties file to find the replacement
