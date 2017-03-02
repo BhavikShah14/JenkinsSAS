7f9e4ffac8780a517e41296425d328c8489c068d
SAS Installer User <sas@sasbap.(none)>
2017-03-02 12:04:56 -0500

    maxdepth 1

--- JenkinsExecutable/importMetadata.sh
+++ JenkinsExecutable/importMetadata.sh
@@ -157,7 +157,8 @@ done
 
 
 # Deploy all other spk except for the ones already deployed in the order above
-for i in $(find $(dirname $(readlink -f $0))/.. -iname  "*.spk" ! -iname "*roles*.spk" ! -iname "*libraries*.spk" ! -iname "*server*.spk" ! -iname "*ACT*.spk" ! -iname "*user*.spk" ! -iname "*usergroup*.spk" ! -iname "*.spkd/*.spk")
+#To deal with spk within exploded spks the find command has maxdepth of 1
+for i in $(find "$(cd ..; pwd)" -maxdepth 1 -iname "*.spk" ! -iname "*roles*.spk" ! -iname "*libraries*.spk" ! -iname "*server*.spk" ! -iname "*ACT*.spk" ! -iname "*user*.spk" ! -iname "*usergroup*.spk")
 do
 	${ImportPackagePath}/ImportPackage -profile "$profile" -package "$i" -target / -preservePaths -includeACL -disableX11 -subprop $i.subprop 
 	RC=$?
