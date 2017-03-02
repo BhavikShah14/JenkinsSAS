a2fa7b45a65caf001959a27a838d90d767b16990
SAS Installer User <sas@sasbap.(none)>
2017-03-02 10:46:06 -0500

    finalise

--- JenkinsExecutable/importMetadata.sh
+++ JenkinsExecutable/importMetadata.sh
@@ -157,7 +157,7 @@ done
 
 
 # Deploy all other spk except for the ones already deployed in the order above
-for i in $(find $(dirname $(readlink -f $0))/.. -iname  "*.spk" ! -iname "*roles*.spk" ! -iname "*libraries*.spk" ! -iname "*servers*.spk" ! -iname "*ACT*.spk" ! -iname "*users*.spk" ! -iname "*usergroups*.spk")
+for i in $(find $(dirname $(readlink -f $0))/.. -iname  "*.spk" ! -iname "*roles*.spk" ! -iname "*libraries*.spk" ! -iname "*server*.spk" ! -iname "*ACT*.spk" ! -iname "*user*.spk" ! -iname "*usergroup*.spk")
 do
 	${ImportPackagePath}/ImportPackage -profile "$profile" -package "$i" -target / -preservePaths -includeACL -disableX11 -subprop $i.subprop 
 	RC=$?
